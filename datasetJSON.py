a64087fe90757ef9cc2742ffcc075759698ea501
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-09 18:27:40 -0500

    Create datasetJSON.py

---
+++ datasetJSON.py
@@ -0,0 +1,11 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+urlJSON = "https://zenodo.org/record/4485741/export/json"
+soup = BeautifulSoup( requests.get(urlJSON).content, "html.parser")
+datasetJSON = json.loads( soup.find("pre", style = "white-space: pre-wrap;").contents[0] )
+datasetURLs = [ {"month":entity["key"].split('_')[1],"key":entity["key"]} for entity in datasetJSON["files"] if "csv.gz" in entity["key"] ]
+
+with open('datasetURLs.json', 'w') as json_file:
+  json.dump(datasetURLs, json_file)
