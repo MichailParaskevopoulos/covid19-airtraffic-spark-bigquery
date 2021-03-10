742438d8fa58fe999f81758b33e9f39ea7dc40e5
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-10 17:08:20 -0500

    Update pyspark_airtraffic.py

--- pyspark_airtraffic.py
+++ pyspark_airtraffic.py
@@ -10,32 +10,24 @@ import warnings
 import re
 
 #inputDir = 'gs://covid19flights/*.csv'
-inputDir = 'gs://covid19flights/{}'
-fileNames = 
+#inputDir = 'gs://covid19flights/{}'
+inputDir = 'gs://covid19flights/flightlist_20190701_20190731.csv'
+
+with open('gs://covid19flights/datasetURLs.json') as json_file:
+  fileNames = json.load(json_file)
 
 def month_of_row(day):
 	day_components = day.split('-')
 	return "{}-{}-01".format(day_components[0],day_components[1])
 
-def main():
+if __name__ == '__main__':
 	scSpark = SparkSession.builder.appName("reading csv").getOrCreate()
 	data_file = inputDir
 	scSpark.conf.set("temporaryGcsBucket","pyspark_output_files")
 	
 	sdfData = scSpark.read.csv(data_file, header=True, sep=",").cache()
 
-
-if __name__ == '__main__':
-
-
-
-	
 	udf_month_of_row = f.udf(month_of_row, StringType())
 	
 	sdfData_with_month = sdfData.withColumn("month", to_date(unix_timestamp(udf_month_of_row("day"), "yyyy-MM-dd").cast("timestamp")))
 	sdfData_with_month.write.format("bigquery").option("table","covid19flights:covid19_airtraffic.count").option("partitionField", "month").mode("append").save()
-	
-
-
-
-	
