f18130024eaa151cb9a7eca901a83cb21a8ab9e0
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-10 10:00:15 -0500

    Update datasetJSON.py

--- datasetJSON.py
+++ datasetJSON.py
@@ -2,11 +2,15 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-urlJSON = "https://zenodo.org/record/4485741/export/json"
-soup = BeautifulSoup( requests.get(urlJSON).content, "html.parser")
-datasetJSON = json.loads( soup.find("pre", style = "white-space: pre-wrap;").contents[0] )
+def main():
+ urlJSON = "https://zenodo.org/record/4485741/export/json"
+ soup = BeautifulSoup( requests.get(urlJSON).content, "html.parser")
+ datasetJSON = json.loads( soup.find("pre", style = "white-space: pre-wrap;").contents[0] )
 
-datasetURLs = [ "https://zenodo.org/record/4485741/files/{}".format(entity["key"]) for entity in datasetJSON["files"] if "csv.gz" in entity["key"] ]
+ datasetURLs = [ "https://zenodo.org/record/4485741/files/{}".format(entity["key"]) for entity in datasetJSON["files"] if "csv.gz" in entity["key"] ]
 
-with open('datasetURLs.json', 'w') as json_file:
- json.dump(datasetURLs, json_file)
+ with open('datasetURLs.json', 'w') as json_file:
+  json.dump(datasetURLs, json_file)
+
+if __name__=="__main__":
+ main()
