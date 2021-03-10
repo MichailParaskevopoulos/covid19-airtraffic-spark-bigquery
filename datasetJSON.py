fe737839c0a974d02caf7c2b427e9a87ff0bcd59
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-09 20:09:04 -0500

    Update datasetJSON.py

--- datasetJSON.py
+++ datasetJSON.py
@@ -5,10 +5,8 @@ from bs4 import BeautifulSoup
 urlJSON = "https://zenodo.org/record/4485741/export/json"
 soup = BeautifulSoup( requests.get(urlJSON).content, "html.parser")
 datasetJSON = json.loads( soup.find("pre", style = "white-space: pre-wrap;").contents[0] )
-#datasetURLs = [ {"month":entity["key"].split('_')[1],"key":entity["key"]} for entity in datasetJSON["files"] if "csv.gz" in entity["key"] ]
+
 datasetURLs = [ "https://zenodo.org/record/4485741/files/{}".format(entity["key"]) for entity in datasetJSON["files"] if "csv.gz" in entity["key"] ]
-datasetURLs = [datasetURLs[0]]
+
 with open('datasetURLs.json', 'w') as json_file:
  json.dump(datasetURLs, json_file)
-
- 
