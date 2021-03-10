337e6ae2867018bc435ea7c74e12eaadcf547a9f
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-09 20:07:49 -0500

    Update envSetup.sh

--- envSetup.sh
+++ envSetup.sh
@@ -1,19 +1,23 @@
 cd
+sudo apt install wget
+
 git clone https://github.com/MichailParaskevopoulos/covid19_airtraffic_data.git
 
 python3 -m venv env
 
 cd covid19_airtraffic_data
-source env/bin/activate
 
+source env/bin/activate
 pip install bs4
 pip install requests
 
 python datasetJSON.py
 
-while read entry; 
-do
-    jq -r '.key' <<< $entry
-done <<< $(jq -c '.[]' datasetURLs.json)
-
-
+export PROJECT_ID='biiibo-proj-2'
+for i in $(jq -r ". | .[]" datasetURLs.json)
+  do
+    wget $i
+    gunzip $(basename $i)
+    gsutil cp $(basename $i .gz) gs://${PROJECT_ID}/covid19_airtraffic_data
+    rm $(basename $i .gz)
+  done
