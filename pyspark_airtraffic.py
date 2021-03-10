858d3170fc688d7b5a43d74142d918a1f340142b
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-10 16:20:06 -0500

    Update pyspark_airtraffic.py

--- pyspark_airtraffic.py
+++ pyspark_airtraffic.py
@@ -9,7 +9,6 @@ from pyspark.sql.types import StringType
 import warnings
 import re
 
-#set input to all csv files of the covid19flights bucket
 #inputDir = 'gs://covid19flights/*.csv'
 inputDir = 'gs://covid19flights/flightlist_20190101_20190131.csv'
 
@@ -25,18 +24,15 @@ if __name__ == '__main__':
 		return "{}-{}-01".format(day_components[0],day_components[1])
 	
 	udf_month_of_row = f.udf(month_of_row, StringType())
-	#sdfData_with_month = sdfData.withColumn("month", udf_month_of_row("day"))
 	
 	sdfData_with_month = sdfData.withColumn("month", to_date(unix_timestamp(udf_month_of_row("day"), "yyyy-MM-dd").cast("timestamp")))
-	#sdfData_with_month.write.format("bigquery").option("partitionField", "month").save("covid19flights:covid19_airtraffic.count")
-	sdfData_with_month.write.format("bigquery").save("covid19flights:covid19_airtraffic.count")
-		
-		
-		
-		
-		
-		
-		
+	sdfData_with_month.write.format("bigquery").option("partitionField", "month").save("covid19flights:covid19_airtraffic.count")
+	
+	
+	
+	
+	#sdfData_with_month.write.format("bigquery").save("covid19flights:covid19_airtraffic.count")
+
 		
 	#sdfData.registerTempTable("airports")
 	#output =  scSpark.sql('SELECT COUNT(destination) as count_destination, origin, destination from airports GROUP BY origin, destination')
