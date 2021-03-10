906adcb824525ac75db0b062c9d104d7f62ba81a
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-10 10:16:47 -0500

    Update envSetup.sh

--- envSetup.sh
+++ envSetup.sh
@@ -3,21 +3,24 @@ sudo apt install wget
 
 git clone https://github.com/MichailParaskevopoulos/covid19_airtraffic_data.git
 
-python3 -m venv env
+apt-get install python3-venv
 
 cd covid19_airtraffic_data
 
+python3 -m venv env
 source env/bin/activate
 pip install bs4
 pip install requests
 
 python datasetJSON.py
 
+gsutil config
+
 export PROJECT_ID='covid19flights'
 for i in $(jq -r ". | .[]" datasetURLs.json)
   do
     wget $i
     gunzip $(basename $i)
-    gsutil cp $(basename $i .gz) gs://${PROJECT_ID}/covid19_airtraffic_data
+    gsutil cp $(basename $i .gz) gs://${PROJECT_ID}
     rm $(basename $i .gz)
   done
