4fd9db6d362169ca364247e58f2b2c36e0fa9f8c
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-11 13:29:39 -0500

    Update pyspark_aircraft_types.py

--- pyspark_aircraft_types.py
+++ pyspark_aircraft_types.py
@@ -3,13 +3,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 
 def main():
-	scSpark = SparkSession.builder.appName("plane_data").getOrCreate()	
+	scSpark = SparkSession.builder.appName("aircraft_types").getOrCreate()	
 	scSpark.conf.set("temporaryGcsBucket","pyspark_output_files")
 	
-	data_file = "gs://openflights/doc8643AircraftTypes.csv"
+	data_file = "gs://covid19flights_dim_tables/doc8643AircraftTypes.csv"
   
 	sdfData = scSpark.read.csv(data_file, header=True, sep=",")
-	sdfData.write.format("bigquery").option("table","covid19flights:covid19_airtraffic.planes").mode("overwrite").save()	
+	sdfData.write.format("bigquery").option("table","covid19flights:covid19_airtraffic.aircraft_types").mode("overwrite").save()	
 
 
 if __name__ == '__main__':
