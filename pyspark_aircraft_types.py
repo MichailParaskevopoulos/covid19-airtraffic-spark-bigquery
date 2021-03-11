daabaf1794ed2684e4356b8220c04ade2bc2a51d
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-11 13:33:07 -0500

    Update pyspark_aircraft_types.py

--- pyspark_aircraft_types.py
+++ pyspark_aircraft_types.py
@@ -9,6 +9,7 @@ def main():
 	data_file = "gs://covid19flights_dim_tables/doc8643AircraftTypes.csv"
   
 	sdfData = scSpark.read.csv(data_file, header=True, sep=",")
+	sdfData.dropDuplicates("Designator")
 	sdfData.write.format("bigquery").option("table","covid19flights:covid19_airtraffic.aircraft_types").mode("overwrite").save()	
 
 
