156c7c1fbff8b69ebc3fbcb22ed0951c16ffc07a
Michail Paraskevopoulos <73489189+MichailParaskevopoulos@users.noreply.github.com>
2021-03-10 19:14:21 -0500

    Update pyspark_airtraffic.py

--- pyspark_airtraffic.py
+++ pyspark_airtraffic.py
@@ -1,6 +1,6 @@
 import pyspark as ps
 from pyspark.sql import functions as f
-from pyspark.sql.functions import col, unix_timestamp, to_date
+from pyspark.sql.functions import regexp_extract, col, unix_timestamp, to_date
 from pyspark.sql import types as t
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
@@ -11,8 +11,6 @@ import math
 import re
 
 inputDir = 'gs://covid19flights/*.csv'
-#inputDir = 'gs://covid19flights/{}'
-#inputDir = "gs://covid19flights/flightlist_20190701_20190731.csv"
 
 def month_of_row(day):
 	day_components = day.split('-')
@@ -34,20 +32,34 @@ def distance_travelled(lat1, lon1, lat2, lon2):
 	distance = R * c
 	return distance
 
+def flight_category(distance):
+	if distance < 1500:
+		category = "short-haul"
+	elif distance > 4000:
+		category = "long-haul"
+	else:
+		category = "medium-haul"
+			
 def preprocessing(dataFrame):
 	udf_month_of_row = f.udf(month_of_row, StringType())
 	udf_distance = f.udf(distance_travelled, FloatType())
+	udf_flight_category = f.udf(flight_category, StringType())
 	
 	#Drop null values
 	dataFrame = dataFrame.na.drop(how='any', thresh=None, subset=["origin","destination","typecode","callsign","latitude_1","longitude_1","latitude_2","longitude_2"])
 	#Create "month" dimension to be used as the partitioning field
 	dataFrame = dataFrame.withColumn("month", to_date(unix_timestamp(udf_month_of_row("day"), "yyyy-MM-dd").cast("timestamp")))
-	
+	#Change column data type
 	for column in ("latitude_1","longitude_1","latitude_2","longitude_2"):
 		dataFrame = dataFrame.withColumn(column, dataFrame[column].cast(FloatType()))
-	
+	#Create column of distance travelled
 	dataFrame = dataFrame.withColumn("distance", udf_distance("latitude_1","longitude_1","latitude_2","longitude_2"))
-	
+	#Create column of flight category
+	dataFrame = dataFrame.withColumn("flight_category", udf_flight_category("distance")
+	#Create column of IATA airline identifier			 
+	dataFrame = dataFrame.withColumn("iata_airline", regexp_extract(col("callsign"), "^[A-Z]{3}", 1))	 
+	#Drop columns
+	dataFrame = dataFrame.drop(*["latitude_1","longitude_1","latitude_2","longitude_2","altitude_1","altitude_2","number","icao24","registration","firstseen","lastseen","day","callsign"])					
 	return dataFrame
 	
 def main():
